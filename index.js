56336967c5162e5cca4ad19c36f37de9d6d4a604
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-04 06:56:00 +0900

    Intelligent traversal.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 
 var resolve = require('resolve');
+var findup = require('findup-sync');
+var path = require('path');
+var cwd = process.cwd();
+
+var configFile = findup('Hackerfile.js', {cwd: cwd});
+if (configFile) {
+  console.log('Found Hackerfile:', configFile);
+  cwd = path.dirname(configFile);
+  console.log('Setting current working directory:', cwd);
+} else {
+  console.log('No Hackerfile found.');
+  process.exit(1);
+}
 
 try {
   var localHacker = resolve.sync('hacker', {basedir: process.cwd()});
